4daf59e35521ee739d7632d5c804879e30d2e183
ChuckBTaylor <WLScripps@gmail.com>
2017-10-30 23:06:32 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,35 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component{
+  render(){
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['salmon', 'eggshell-white', 'white']).isRequired,
+  weight: function(props, propName, componentName){
+    if(props[propName] === undefined){
+      return new Error("")
+    }
+    if(isNaN(props[propName])){
+      return new Error("")
+    }
+    if(props[propName] > 300 || props[propName] < 80){
+      return new Error("")
+    }
+  }
+};
+
+export default Product;
